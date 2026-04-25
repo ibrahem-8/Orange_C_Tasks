10252fa60949ff3376cc3282dadbd731e2f9fb1f
ibrahem-8 <159235353+ibrahem-8@users.noreply.github.com>
2026-04-25 09:58:50 +0300

    Object-Oriented Programming (OOP) in C#

---
+++ C# Tasks - (Methods,List,LINQ,LinkedList)/C# Tasks - (Methods,List,LINQ,LinkedList)/Program.cs
@@ -0,0 +1,227 @@
+﻿using System.Linq;
+//*Task 1
+
+//List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+//List<int> evenNumbers = numbers.Where(n => n % 2 == 0).ToList();
+
+//foreach (var num in evenNumbers)
+//{
+//    Console.WriteLine(num);
+//}
+
+/////////////////////////////////////////////////////////////////////////
+//*Task 2
+
+//List<int> numbers = new List<int> { 1, 2, 3, 4, 2, 6, 1, 3 };
+
+//numbers = numbers.Distinct().ToList();
+
+//foreach (var num in numbers)
+//{
+//    Console.WriteLine(num);
+//}
+
+/////////////////////////////////////////////////////////////////////////
+//*Task 3
+
+//List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+//int max = numbers.Max();
+//Console.WriteLine($"The maximum number is: {max}");
+
+/////////////////////////////////////////////////////////////////////////
+//*Task 4
+
+//List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+//var reversed = numbers.Reverse();// *why error?
+//numbers.Reverse();
+//foreach (var num in numbers)
+//{
+//    Console.WriteLine(num);
+//}
+
+/////////////////////////////////////////////////////////////////////////
+//*Task 5
+
+//List<int> numbers = new List<int> { 50, 30, 78, 90, 111 };
+
+//var greater50 = numbers.Where(n => n > 50);
+
+//foreach (var num in greater50)
+//{
+//    Console.WriteLine(num);
+//}
+
+/////////////////////////////////////////////////////////////////////////
+//*Task 6
+
+//Dictionary<string, int> nameScore = new Dictionary<string, int>
+//{
+//    { "Alice", 30 },
+//    { "Bob", 88 },
+//    { "Charlie", 100 },
+//    { "David", 28 }
+//};
+
+//var highScorers = nameScore.Where(kv => kv.Value > 80);
+
+//foreach (var kv in highScorers)
+//{
+//    Console.WriteLine($"{kv.Key}: {kv.Value}");
+//}
+
+///////////////////////////////////////////////////////////////////////
+//*Task 7 (with)
+
+//Dictionary<string, int> nameScore = new Dictionary<string, int>
+//{
+//    { "Alice", 30 },
+//    { "Bob", 88 },
+//    { "Charlie", 100 },
+//    { "David", 28 }
+//};
+
+//Console.WriteLine("enter the name");
+//string name = Console.ReadLine();
+
+//if (nameScore.TryGetValue(name, out int score))
+//{
+//    Console.WriteLine($"{name}: {score}");
+//}
+//else
+//{
+//    Console.WriteLine("Name not found.");
+//}
+
+////////////////////////////////////////////////////////////////////////////
+//*Task 8
+
+//Dictionary<string, int> nameScore = new Dictionary<string, int>
+//{
+//    { "Alice", 30 },
+//    { "Bob", 88 },
+//    { "Charlie", 100 },
+//    { "David", 28 }
+//};
+
+//int totalScore = nameScore.Values.Sum();
+//Console.WriteLine($"Total Score: {totalScore}");
+
+////////////////////////////////////////////////////////////////////////////
+//*Task 9
+
+//Dictionary<string, int> nameScore = new Dictionary<string, int>
+//{
+//    { "Alice", 30 },
+//    { "Bob", 88 },
+//    { "Charlie", 100 },
+//    { "David", 28 }
+//};
+
+//foreach (var item in nameScore)
+//{
+//    if (item.Value < 50)
+//    {
+//        nameScore.Remove(item.Key);
+//    }
+//}
+
+//foreach (var item in nameScore)
+//{
+//    Console.WriteLine($"{item.Key}: {item.Value}");
+//}
+
+///////////////////////////////////////////////////////////////////////////
+//*Task 10
+
+//Dictionary<string, int> nameScore = new Dictionary<string, int>
+//{
+//    { "Alice", 30 },
+//    { "Bob", 88 },
+//    { "Charlie", 100 },
+//    { "David", 28 }
+//};
+
+//var topStudent = nameScore.OrderByDescending(kv => kv.Value).First();
+//Console.WriteLine($"Top Student: {topStudent.Key} with score {topStudent.Value}");
+
+///////////////////////////////////////////////////////////////////////////
+//*Task 11 + 12 + 13 + 14 + 15
+
+//LinkedList<string> pages = new LinkedList<string>();
+
+//pages.AddFirst("Page first");
+//pages.AddLast("Page second");
+//pages.AddLast("Page last");
+
+//pages.Remove("Page second");
+
+//pages.AddAfter(pages.First, "Page after first");
+//pages.AddBefore(pages.Last, "Page before last");
+
+//foreach (var page in pages)
+//{
+//    Console.WriteLine(page);
+//}
+
+//Console.WriteLine(pages.First());
+//Console.WriteLine(pages.Last());
+
+///////////////////////////////////////////////////////////////////////////
+//*Task 16
+
+//List<int> ints = new List<int>() {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+//var oddNums = ints.Where(n => n % 2 != 0);
+
+//foreach (var num in oddNums)
+//{
+//    Console.WriteLine(num);
+//}
+
+//////////////////////////////////////////////////////////////////////////////
+//*Task 17
+
+//List<int> ints = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+//var multiNums = ints.Select(n => n * 2);
+
+//foreach (var num in multiNums)
+//{
+//    Console.WriteLine(num);
+//}
+
+///////////////////////////////////////////////////////////////////////////
+//*Task 18
+
+//List<int> ints = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+//var sortedDec = ints.OrderByDescending(n => n);
+
+//foreach (var num in sortedDec)
+//{
+//    Console.WriteLine(num);
+//}
+
+///////////////////////////////////////////////////////////////////////////
+//*Task 19
+
+//List<int> ints = new List<int>() { 10, 500, -20, 77, 80, 998, 100 };
+
+//int fNumGreaterThan100 = ints.Where(n => n > 100).FirstOrDefault();
+
+//Console.WriteLine(fNumGreaterThan100);
+
+///////////////////////////////////////////////////////////////////////////
+//*Task 20
+
+//List<int> ints = new List<int>() { 10, 500, -20, 77, 80, 998, 100 };
+
+//double avg = ints.Average();
+//Console.WriteLine($"Average: {avg}");
+
+///////////////////////////////////////////////////////////////////////////
+//*Task 21
+
---
+++ Object-Oriented Programming (OOP) in C#/Object-Oriented Programming (OOP) in C#/Program.cs
@@ -0,0 +1,112 @@
+﻿//*Task 1
+
+//class person
+//{
+//    public string name { get; set;}
+//    private int age { get; set; }
+//    public person(string name, int age)
+//    {
+//        this.name = name;
+//        this.age = age;
+//    }
+//    public void display()
+//    {
+//        Console.WriteLine("Name: " + name);
+//        Console.WriteLine("Age: " + age);
+//    }
+//    ~person()
+//    {
+//        Console.WriteLine("Destructor called for " + name);
+//    }
+//}
+
+//class student : person
+//{
+//    public student(string name, int age) : base(name, age)
+//    {
+//    }
+//    public void study()
+//    {
+//        Console.WriteLine(name + " is studying.");
+//    }
+//}
+
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        student s1 = new student("Alice", 20);
+//        s1.display();
+//        s1.study();
+//        student s2 = new student("Bob", 22);
+//        s2.display();
+//        s2.study();
+//    }
+//}
+
+//////////////////////////////////////////////////////////////////////////////
+//*Task 2
+
+//using System.Diagnostics;
+//using System.Drawing;
+//using System.Reflection;
+
+//class car
+//{
+//    public string make { get; set; }
+//    public int year { get; set; }
+//    public string type { get; set; }
+//    public double price { get; set; }
+//    public string model { get; set; }
+//    public string palletNo { get; set; }
+//    public string color { get; set; }
+//    public bool isEngineRunning;
+
+//    public car(string make, int year, string type, double price,
+//               string model, string palletNo, string color)
+//    {
+//        this.make = make;
+//        this.year = year;
+//        this.type = type;
+//        this.price = price;
+//        this.model = model;
+//        this.palletNo = palletNo;
+//        this.color = color;
+//        isEngineRunning = false;
+//    }
+
+//    public void StartEngine()
+//    {
+//        isEngineRunning = true;
+//        Console.WriteLine("Engine started.");
+//    }
+
+//    public void StopEngine()
+//    {
+//        isEngineRunning = false;
+//        Console.WriteLine("Engine stopped.");
+//    }
+
+//    public void DisplayInfo()
+//    {
+//        Console.WriteLine("Make: " + make);
+//        Console.WriteLine("Year: " + year);
+//        Console.WriteLine("Type: " + type);
+//        Console.WriteLine("Price: " + price);
+//        Console.WriteLine("Model: " + model);
+//        Console.WriteLine("Pallet No: " + palletNo);
+//        Console.WriteLine("Color: " + color);
+//    }
+
+//}
+
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        car c1 = new car("Toyota", 2020, "Sedan", 20000, "Camry", "ABC123", "Red");
+//        c1.DisplayInfo();
+//        c1.StartEngine();
+//        c1.StopEngine();
+//    }
+//}
